3084c05242af7adda935bb24ec4879cff212bf66
CarlLienert <63995610+CarlLienert@users.noreply.github.com>
2026-01-09 17:19:04 +0000

    Latest build deployed.

---
+++ notes/lunr-pretext-search-index.js
@@ -0,0 +1,59 @@
+var ptx_lunr_search_style = "textbook";
+var ptx_lunr_docs = [
+{
+  "id": "Section_1_1",
+  "level": "1",
+  "url": "Section_1_1.html",
+  "type": "Section",
+  "number": "",
+  "title": "Section 1.1",
+  "body": " Section 1.1   Problem 6   For : and so the inequality holds. Assume and consider: The second inequality requires that we prove for . The following inequalities are equivalent: The last inequality is true if or . So, for . We have shown that implies , and we have shown the inequality is true for . By induction, is true for all integers .      Problem 8  For : Assume the identity is true for and consider: which shows the identity is true for . By induction the identity is true for all integers . The inequality is equivalent to so from part (a), we need to show that The right hand side is: Clearly since there are factors in both products and all but the first factor are greater in the right hand side. For example, if : Multiply both sides of by to obtain the desired result.    Problem 9  For , . Assume and consider : The first inequality follows from the inductive hypothesis. This first inequality is also where we need to use the assumption that . If , the direction of the inequality would change. The second inequality is true since .    Problem 10  (a) The following is not a complete proof or explanation. It is only the algebra part. Make sure you can identify where the induction hypothesis is used. Make sure you can justify both of the inequalities. (b) Again, the following is only the algebra. It does not constitute a complete proof.     "
+},
+{
+  "id": "Section_1_2",
+  "level": "1",
+  "url": "Section_1_2.html",
+  "type": "Section",
+  "number": "",
+  "title": "Section 1.2",
+  "body": " Section 1.2    Problem 3       Similar idea: let and in the binomial theorem.     Use the expansion of with .  Follow the hint. Make sure to justify both equalities.         Problem 7  Use induction:     Problem 8       Problem 9  Using #8, this is equivalent to: Consider the fraction as: Each of these fractions is less than one, so the product is less than one, which gives the right hand inequality. With the exception of , each of these fractions is greater than . There are factors so the product is greater than .   "
+},
+{
+  "id": "Section_2_1",
+  "level": "1",
+  "url": "Section_2_1.html",
+  "type": "Section",
+  "number": "",
+  "title": "Section 2.1",
+  "body": " Section 2.1   Problem 1                 Problem 3  The hint follows from question 1(a). If is even, let : where we have used the result from section 1.1 concerning the sum of the first squares. If is odd:      Problem 7        Problem 10  Use induction. The key algebraic step is:     Problem 11              "
+},
+{
+  "id": "Section_2_2",
+  "level": "1",
+  "url": "Section_2_2.html",
+  "type": "Section",
+  "number": "",
+  "title": "Section 2.2",
+  "body": " Section 2.2   Problem 1  From the division algorithm, can be written uniquely as where . We'll add and subtract : For the inequality we have:     Problem 2  Suppose . Then, and so is of the form where . Conversely, for example, is not of the form since has no integer solutions.    Problem 3  By the division algorithm, any integer, , is of the form , , or . Square each of these cases: So the square of any integer is of the form or     Problem 4  If is an integer then is of the form or by problem 3a. If then: which is neither of the form or . (It is of the form .) If then: Again, this is neither of the form or . Since is never of the form or it cannot be a square.    Problem 6  Let by the division algorithm. There are 6 cases. Below the results are shown for some . For example on the first line .     Problem 9  Let and write where . Consider the squares in each of the cases: ( represents some integer below. For example would be in the first line.) And, similarly, the cubes: So, squares of integers are of the form , , , or . Cubes are of the form , , or . If an integer is both a square and a cube it must be of the form or .    Problem 10  There are 6 cases. We'll illustrate here with one case: say . Rather than deal with the entire expression let's try to build one piece at a time: This proves the result. It's a bit tedious to find each of the integers but not computationally difficult. For this problem we are only concerned that such integers exist, not what they are.   "
+},
+{
+  "id": "Section_2_3",
+  "level": "1",
+  "url": "Section_2_3.html",
+  "type": "Section",
+  "number": "",
+  "title": "Section 2.3",
+  "body": " Section 2.3   Problem 2    If , there exists such that . Then so .    If and then there exist integers such that and . Multiply these two equations: , so .    If then for some integer . Then so . Conversely, if then for some integer . Divide by to obtain , so .     "
+}
+]
+
+var ptx_lunr_idx = lunr(function () {
+  this.ref('id')
+  this.field('title')
+  this.field('body')
+  this.metadataWhitelist = ['position']
+
+  ptx_lunr_docs.forEach(function (doc) {
+    this.add(doc)
+  }, this)
+})
